c3069cb32b2b7074df070faec4b3311ac049482b
unknown <rmtestard@gmail.com>
2019-06-04 13:23:15 +0200

    responsive background done

--- src/components/HomeImage.js
+++ src/components/HomeImage.js
@@ -3,8 +3,8 @@ import Landscape from '../static/images/home.jpg';
 
 const HomeImage = () => {
   return (
-    <div>
-      <img alt="Landscape" src={Landscape} className=".homeImage" />
+    <div className="homeImageWrapper">
+      <img alt="Landscape" src={Landscape} className="homeImage" />
     </div>
   );
 };
