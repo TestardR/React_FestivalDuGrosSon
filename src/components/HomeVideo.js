77b0caf06a2e03bb6dd4ec22122236fca4e7a384
unknown <rmtestard@gmail.com>
2019-06-04 18:14:12 +0200

    homeLogo done

--- src/components/HomeVideo.js
+++ src/components/HomeVideo.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Video from '../static/videos/homepage_video.mp4';
-import Clementine from '../static/images/clementine.jpg';
 
-const HomeVideo = () => {
-  return (
-    <div className="homeVideo">
-      <img alt="Clementine" src={Clementine} className="homeLogo" />
-      <video loop autoPlay>
-        <source src={Video} type="video/mp4" />
-      </video>
-    </div>
-  );
-};
+class HomeVideo extends Component {
+  render() {
+    return (
+      <div className="homeVideo">
+        <div className="homeLogo">
+          <div className="title">{`Festival 
+          du 
+          Graux Son`}</div>
+          <div className="edition info">2nd edition</div>
+          <div className="info">
+            {`October 11-12-13, 2019
+          Tournai, Belgique`}
+          </div>
+        </div>
+        <video loop autoPlay>
+          <source src={Video} type="video/mp4" />
+        </video>
+      </div>
+    );
+  }
+}
 
 export default HomeVideo;
