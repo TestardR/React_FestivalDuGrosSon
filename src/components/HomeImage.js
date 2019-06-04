9a91e7bc778a85df31e0e8bbc9dc4468dad3bc72
unknown <rmtestard@gmail.com>
2019-06-04 18:24:09 +0200

    homeLogo responsive done

--- src/components/HomeImage.js
+++ src/components/HomeImage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Landscape from '../static/images/home.jpg';
+import HomeLogo from './HomeLogo';
 
 const HomeImage = () => {
   return (
     <div>
+      <HomeLogo />
       <img alt="Landscape" src={Landscape} className="homeImage" />
     </div>
   );
---
+++ src/components/HomeLogo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const HomeLogo = () => {
+  return (
+    <div className="homeLogo">
+      <div className="title">{`Festival 
+        du 
+        Graux Son`}</div>
+      <div className="edition info">2nd edition</div>
+      <div className="info">
+        {`October 11-12-13, 2019
+        Tournai, Belgique`}
+      </div>
+    </div>
+  );
+};
+
+export default HomeLogo;
