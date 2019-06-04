eb8206c26de62820f55e0b9c586940731326c906
unknown <rmtestard@gmail.com>
2019-06-04 13:26:14 +0200

    home component routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home';
 
 function App() {
   return (
     <Router>
-      <Home />
+      <Route path="/home" exact component={Home} />
     </Router>
   );
 }
